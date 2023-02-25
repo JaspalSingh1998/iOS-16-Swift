f76fa32d29772bcfdb6b9362fb80f2a1d46f8ddf
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-02-25 11:02:45 -0500

    Finished Profile Application and building slot-machine application now

---
+++ 01-Profile-App/01-Profile-App/_1_Profile_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_Profile_AppApp.swift
+//  01-Profile-App
+//
+//  Created by Jaspal Singh on 2023-02-24.
+//
+
+import SwiftUI
+
+@main
+struct _1_Profile_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ slot-machine/slot-machine/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  slot-machine
+//
+//  Created by Jaspal Singh on 2023-02-25.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
