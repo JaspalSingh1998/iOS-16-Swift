b0a7569b604fa3961730d68b15ecaecc5946d933
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-02-28 11:13:21 -0500

    Finished Calculator Application

--- CalculatorWithAnimation/CalculatorWithAnimation/Views/KeyViews.swift
+++ CalculatorWithAnimation/CalculatorWithAnimation/Views/KeyViews.swift
@@ -27,7 +27,7 @@ struct KeyViews: View {
             HStack{
                 RoundedRectangle(cornerRadius: 15)
                     .foregroundColor(changeColor ? Color("Num").opacity(0.4) : Color.pink.opacity(0.2))
-                    .scaleEffect(changeColor ? 1.5 : 1.0)
+                    .scaleEffect(changeColor ? 1.1 : 1.0)
                     .frame(width: 350, height: 280)
                     .animation(Animation.easeInOut.speed(0.17).repeatForever(), value: changeColor)
                     .onAppear(perform: {
@@ -42,14 +42,14 @@ struct KeyViews: View {
                 HStack(spacing: 10) {
                     ForEach(row, id: \.self){elem in
                         Button {
-                            didTap(button: elem)
+                            self.didTap(button: elem)
                         } label: {
                             Text(elem.rawValue)
                                 .font(.system(size: 30))
-                                .frame(width: getWidth(elem: elem), height: getHeight(elem: elem))
+                                .frame(width: self.getWidth(elem: elem), height: self.getHeight(elem: elem))
                                 .background(elem.buttonColor)
                                 .foregroundColor(.black)
-                                .cornerRadius(getWidth(elem: elem) / 2)
+                                .cornerRadius(self.getWidth(elem: elem) / 2)
                                 .shadow(color: .purple.opacity(0.8), radius: 30)
                         }
                     }
@@ -57,23 +57,69 @@ struct KeyViews: View {
             }
         }
     }
-}
-
-func getWidth(elem: Keys) -> CGFloat {
     
-    if elem == .zero {
-        return (UIScreen.main.bounds.width - (5*10)) / 2
+    func getWidth(elem: Keys) -> CGFloat {
+        
+        if elem == .zero {
+            return (UIScreen.main.bounds.width - (5*10)) / 2
+        }
+        
+        return (UIScreen.main.bounds.width - (5*10)) / 4
+    }
+
+    func getHeight(elem: Keys) -> CGFloat {
+        return (UIScreen.main.bounds.width - (5*10)) / 5
     }
     
-    return (UIScreen.main.bounds.width - (5*10)) / 4
-}
-
-func getHeight(elem: Keys) -> CGFloat {
-    return (UIScreen.main.bounds.width - (5*10)) / 5
-}
-
-func didTap(button: Keys){
-    print("Hello Goldy")
+    func didTap(button: Keys){
+        switch button {
+        case .add, .subtract, .multiply, .divide, .equal:
+            if button == .add {
+                self.currentOperation = .add
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .subtract {
+                self.currentOperation = .subtract
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .multiply {
+                self.currentOperation = .multiply
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .divide {
+                self.currentOperation = .divide
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .equal {
+                let runningValue = self.runningNumber
+                let currentValue = Int(self.value) ?? 0
+                
+                switch self.currentOperation {
+                case .add: self.value = "\(runningValue + currentValue)"
+                case .subtract: self.value = "\(runningValue - currentValue)"
+                case .multiply: self.value = "\(runningValue * currentValue)"
+                case .divide: self.value = "\(runningValue / currentValue)"
+                case .none:
+                    break
+                }
+            }
+            if button != .equal{
+                self.value = "0"
+            }
+        case .clear:
+            self.value = "0"
+        case .decimal, .negative, .percentage:
+            break
+        default:
+            let number = button.rawValue
+            if self.value == "0" {
+                value = number
+            }
+            else {
+                self.value = "\(self.value)\(number)"
+            }
+        }
+    }
 }
 
 struct KeyViews_Previews: PreviewProvider {
