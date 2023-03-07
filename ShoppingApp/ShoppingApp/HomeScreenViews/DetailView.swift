78945d94cc6362267da85d64e986e9ea179e4a8f
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-03-07 11:18:14 -0500

    Added Horizontal Scroll For Top Selling section

---
+++ ShoppingApp/ShoppingApp/HomeScreenViews/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  ShoppingApp
+//
+//  Created by Jaspal Singh on 2023-03-07.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
